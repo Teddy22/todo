78a962f391b311dc6d9d41332fd78bd08f0e8cd9
Divya Agarwal <diagarwa@microsoft.com>
2018-12-28 16:27:42 -0800

    update

--- index.js
+++ index.js
@@ -8,3 +8,23 @@ AWS.config.update({
     accessKeyId: process.env.AWSAccessKeyId,
     secretAccessKey: process.env.AWSSecretKey
 });
+
+// create objects in the bucket
+const bucketName = 'todo-123';
+const s3 = new AWS.S3();
+
+s3.listBuckets((err, data)=> {
+    if(err) console.log('error:', err);
+    else console.log(JSON.stringify(data, null, 4));
+});
+
+var objparams = {
+    Body: "Hellohello", 
+    Bucket: "examplebucket", 
+    Key: "objectkey"
+   };
+
+s3.putObject(objparams, (err,data)=>{
+    if(err) console.log('error:', err);
+    else console.log(JSON.stringify(data))
+});
